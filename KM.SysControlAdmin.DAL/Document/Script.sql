a09315699998a2f34b01202f51b674c49686968f
Hector Garcia <hg425823@gmail.com>
2025-05-03 15:15:57 -0600

    ACTUALIZACION DE SCRIPT.SQL

--- KM.SysControlAdmin.DAL/Document/Script.sql
+++ KM.SysControlAdmin.DAL/Document/Script.sql
@@ -10,10 +10,30 @@ CREATE TABLE [Role](
 	DateModification DATETIME NOT NULL
 );
 GO
-    INSERT INTO [Role] VALUES('Desarrollador');
-	INSERT INTO [Role] VALUES('Administrador');
-	INSERT INTO [Role] VALUES('Instructor');
-    INSERT INTO [Role] VALUES('Alumno/a');
-    INSERT INTO [Role] VALUES('Secretario/a');
-	INSERT INTO [Role] VALUES('Invitado');
+	INSERT INTO [Role] ([Name], [Status], DateCreated, DateModification) 
+	VALUES('Desarrollador', 1, SYSDATETIME(), SYSDATETIME());
+	INSERT INTO [Role] ([Name], [Status], DateCreated, DateModification)
+	VALUES('Administrador', 1, SYSDATETIME(), SYSDATETIME());
+	INSERT INTO [Role] ([Name], [Status], DateCreated, DateModification)
+	VALUES('Instructor', 1, SYSDATETIME(), SYSDATETIME());
+	INSERT INTO [Role] ([Name], [Status], DateCreated, DateModification)
+	VALUES('Alumno/a', 1, SYSDATETIME(), SYSDATETIME());
+	INSERT INTO [Role] ([Name], [Status], DateCreated, DateModification)
+	VALUES('Secretario/a', 1, SYSDATETIME(), SYSDATETIME());
+	INSERT INTO [Role] ([Name], [Status], DateCreated, DateModification)
+	VALUES('Invitado', 1, SYSDATETIME(), SYSDATETIME());
+GO
+CREATE TABLE [User](
+    Id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    [Name] VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    [Password] VARCHAR(32) NOT NULL,
+    [Status] TINYINT NOT NULL,
+    RecoveryEmail VARCHAR(50) NOT NULL,
+    DateCreated DATETIME NOT NULL,
+    DateModification DATETIME NOT NULL,
+	ImageData VARBINARY(MAX) NOT NULL,
+	IdRole INT NOT NULL FOREIGN KEY REFERENCES [Role](Id) ON DELETE CASCADE
+);
 GO
