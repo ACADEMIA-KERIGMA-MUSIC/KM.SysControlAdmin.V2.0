c1649cfbf2107c2143b6ae6ebcf68cfa08e5bf96
Hector Garcia <hg425823@gmail.com>
2025-05-06 08:15:10 -0600

    LOGICA DAL, BL, CONTROLLER Y VISTA PARA MOSTRAR INDEX DE MODIFICAR CONTRASEÑA A USUARIOS EXTERNOS

---
+++ KM.SysControlAdmin.WebApp/Controllers/Support - Controller/SupportController.cs
@@ -0,0 +1,39 @@
+﻿#region REFERENCIAS
+// Referencias Necesarias Para El Correcto Funcionamiento
+using KM.SysControlAdmin.BL.Role___BL;
+using KM.SysControlAdmin.BL.User___BL;
+using KM.SysControlAdmin.EN.User___EN;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+#endregion
+
+namespace KM.SysControlAdmin.WebApp.Controllers.Support___Controller
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Roles = "Desarrollador, Administrador")]
+    public class SupportController : Controller
+    {
+        // Creamos Las Instancias Para Acceder a Los Metodos
+        UserBL userBL = new UserBL();
+        RoleBL roleBL = new RoleBL();
+
+        #region METODO PARA INDEX
+        // Metodo Para Mostrar La Vista Index
+        [Authorize(Roles = "Desarrollador, Administrador")]
+        public async Task<IActionResult> Index(User user = null!)
+        {
+            if (user == null)
+                user = new User();
+
+            var users = await userBL.SearchIncludeRoleAsync(user);
+            var roles = await roleBL.GetAllAsync();
+
+            ViewBag.Roles = roles;
+
+            return View(users);
+        }
+        #endregion
+    }
+}
