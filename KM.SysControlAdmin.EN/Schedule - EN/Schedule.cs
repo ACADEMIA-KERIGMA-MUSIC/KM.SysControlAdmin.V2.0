18e65eeaaaf23c871043844fa3b7d7b6602ccd84
Hector Garcia <hg425823@gmail.com>
2025-05-07 12:17:01 -0600

    CREACION DE ENTIDAD SCHEDULE

---
+++ KM.SysControlAdmin.EN/Schedule - EN/Schedule.cs
@@ -0,0 +1,62 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+#endregion
+
+namespace KM.SysControlAdmin.EN.Schedule___EN
+{
+    public class Schedule
+    {
+        #region ATRIBUTOS DE LA ENTIDAD
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "La hora de inicio es requerida")]
+        [Display(Name = "Hora de Inicio")]
+        [DataType(DataType.Time)]
+        public TimeSpan StartTime { get; set; }
+
+        [Required(ErrorMessage = "La hora de fin es requerida")]
+        [Display(Name = "Hora de Finalización")]
+        [DataType(DataType.Time)]
+        public TimeSpan EndTime { get; set; }
+
+        [Required(ErrorMessage = "El Estado Es Requerido")]
+        [Display(Name = "Estado")]
+        public byte Status { get; set; }
+
+        [Display(Name = "Fecha De Creación")]
+        public DateTime DateCreated { get; set; }
+
+        [Display(Name = "Fecha De Modificación")]
+        public DateTime DateModification { get; set; }
+        #endregion
+
+        #region ATRIBUTOS NO MAPEABLES
+        // Propiedad para formatear la hora automáticamente
+        [NotMapped]
+        public string StartTimeFormatted => StartTime.ToString(@"hh\:mm");
+        [NotMapped]
+        public string EndTimeFormatted => EndTime.ToString(@"hh\:mm");
+
+        // Propiedad para formatear la fecha automáticamente
+        [NotMapped]
+        public string DateCreatedFormatted => DateCreated.ToString(@"dd/MM/yyyy");
+        [NotMapped]
+        public string DateModificationFormatted => DateModification.ToString(@"dd/MM/yyyy");
+
+        // Propiedad para formatear la hora con AM/PM
+        [NotMapped]
+        public string TimeCreatedFormatted => DateCreated.ToString("hh:mm tt");
+        [NotMapped]
+        public string TimeModificationFormatted => DateModification.ToString("hh:mm tt");
+        #endregion
+    }
+}
