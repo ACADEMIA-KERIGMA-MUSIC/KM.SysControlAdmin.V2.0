fb37b87e70cb8d82be7ad3cb0b7df80176e89da8
Hector Garcia <hg425823@gmail.com>
2025-05-08 08:21:50 -0600

    CREACION DE ENTIDAD TRAINER

---
+++ KM.SysControlAdmin.EN/Trainer - EN/Trainer.cs
@@ -0,0 +1,163 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using KM.SysControlAdmin.EN.User___EN;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+#endregion
+
+namespace KM.SysControlAdmin.EN.Trainer___EN
+{
+    public class Trainer
+    {
+        #region ATRIBUTOS DE LA ENTIDAD
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El Codigo Es Requerido")]
+        [StringLength(20, ErrorMessage = "Maximo 20 caracteres")]
+        [Display(Name = "Codigo")]
+        public string Code { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Nombre Es Requerido")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        [Display(Name = "Nombres")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", ErrorMessage = "Debe contener solo Letras")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Apellido Es Requerido")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        [Display(Name = "Apellidos")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", ErrorMessage = "Debe contener solo Letras")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Dui Es Requerido")]
+        [StringLength(10, ErrorMessage = "Maximo 10 caracteres")]
+        [Display(Name = "Dui")]
+        public string Dui { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La Fecha De Nacimiento Es Requerida")]
+        [Display(Name = "Fecha De Nacimiento")]
+        [DataType(DataType.Date, ErrorMessage = "Por favor, Introduce una fecha válida")]
+        public DateTime DateOfBirth { get; set; } = DateTime.MinValue;
+
+        [Required(ErrorMessage = "La Edad Es Requerida")]
+        [StringLength(3, ErrorMessage = "Maximo 3 caracteres")]
+        [Display(Name = "Edad")]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "La edad debe contener solo números")]
+        public string Age { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Genero Es Requerido")]
+        [StringLength(20, ErrorMessage = "Maximo 20 caracteres")]
+        [Display(Name = "Genero")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", ErrorMessage = "Debe contener solo Letras")]
+        public string Gender { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Estado Civil Es Requerido")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        [Display(Name = "Estado Civil")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ/ ]+$", ErrorMessage = "Debe contener solo Letras")]
+        public string CivilStatus { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Telefono Es Requerido")]
+        [StringLength(9, ErrorMessage = "Maximo 9 caracteres")]
+        [Display(Name = "Telefono")]
+        [RegularExpression("^[0-9-]+$", ErrorMessage = "El Telefono debe contener solo números")]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La Direccion Es Requerida")]
+        [StringLength(100, ErrorMessage = "Maximo 100 caracteres")]
+        [Display(Name = "Direccion De Residencia")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Estado Es Requerido")]
+        [Display(Name = "Estado")]
+        public byte Status { get; set; }
+
+        [StringLength(300, ErrorMessage = "Maximo 300 caracteres")]
+        [Display(Name = "Comentarios u Observaciones")]
+        public string? CommentsOrObservations { get; set; }
+
+        [Display(Name = "Fecha De Creación")]
+        public DateTime DateCreated { get; set; }
+
+        [Display(Name = "Fecha De Modificación")]
+        public DateTime DateModification { get; set; }
+
+        [Display(Name = "Fotografia")]
+        public byte[]? ImageData { get; set; }
+
+        [Required(ErrorMessage = "La Fecha De Ingreso Es Requerida")]
+        [Display(Name = "Fecha De Ingreso")]
+        [DataType(DataType.Date, ErrorMessage = "Por favor, Introduce una fecha válida")]
+        public DateTime EntryDate { get; set; } = DateTime.MinValue;
+
+        [Required(ErrorMessage = "El Correo Personal es requerido")]
+        [MaxLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        [Display(Name = "Correo Electronico")]
+        public string PersonalEmail { get; set; } = string.Empty;
+
+        #endregion
+
+        #region ATRIBUTOS NO MAPEABLES
+        // Propiedad para formatear la fecha automáticamente
+        [NotMapped]
+        public string DateOfBirthFormatted => DateOfBirth.ToString(@"dd/MM/yyyy");
+        [NotMapped]
+        public string DateCreatedFormatted => DateCreated.ToString(@"dd/MM/yyyy");
+        [NotMapped]
+        public string DateModificationFormatted => DateModification.ToString(@"dd/MM/yyyy");
+        [NotMapped]
+        public string EntryDateFormatted => EntryDate.ToString(@"dd/MM/yyyy");
+
+        // Propiedad para formatear la hora con AM/PM
+        [NotMapped]
+        public string TimeCreatedFormatted => DateCreated.ToString("hh:mm tt");
+        [NotMapped]
+        public string TimeModificationFormatted => DateModification.ToString("hh:mm tt");
+        #endregion
+
+        #region ATRIBUTOS NO MAPEABLES EXTERNOS
+        [NotMapped]
+        [Required(ErrorMessage = "El Correo Electronico es requerido")]
+        [MaxLength(50, ErrorMessage = "Máximo 30 caracteres")]
+        [Display(Name = "Correo Electronico")]
+        public string Email { get; set; } = string.Empty;
+
+        [NotMapped]
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [DataType(DataType.Password)]
+        [StringLength(32, ErrorMessage = "La contraseña debe estar entre 6 y 32 caracteres", MinimumLength = 6)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; } = string.Empty;
+
+        [NotMapped]
+        [ForeignKey("Role")]
+        [Required(ErrorMessage = "El rol es requerido")]
+        [Display(Name = "Rol")]
+        public int IdRole { get; set; }
+
+        [NotMapped]
+        [Required(ErrorMessage = "La confirmación de la contraseña es requerida")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden")]
+        [StringLength(32, ErrorMessage = "La contraseña debe tener entre 6 y 32 caracteres", MinimumLength = 6)]
+        [Display(Name = "Confirmar Contraseña")]
+        public string ConfirmPassword_Aux { get; set; } = string.Empty; //propiedad auxiliar
+
+        [NotMapped]
+        [Required(ErrorMessage = "El Correo De Recuperacion es requerido")]
+        [MaxLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        [Display(Name = "Correo De Recuperacion")]
+        public string RecoveryEmail { get; set; } = string.Empty;
+
+        [NotMapped]
+        public User? User { get; set; } //propiedad de navegación
+        #endregion
+    }
+}
